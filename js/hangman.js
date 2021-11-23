ffb76bd3cbb264e34a1b53f8f2b63a15ed41c3dc
paulinaeriksson <paulina.eriksson@chasacademy.se>
2021-11-23 13:12:53 +0100

    add keybord by using loop

--- js/hangman.js
+++ js/hangman.js
@@ -11,17 +11,38 @@ let dogs = [
     "chihuahua"
 ]
 
-let answer = '';
+let answer = "";
 let maxWrong = 10;
 let mistakes = 0;
+let word = null;
 let guessed = [];
+let letters = "";
 
-
+let alphabet = [
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "å", "ä", "ö"
+]
 
 //Funktion för att slumpa fram ord (hundras). Multiplicera med längden på arrayen för att en ny hundras ska slumpas fram varje gång.
 function random(){
     answer = dogs[Math.floor(Math.random() * dogs.length)];
-    alert(answer);
 }
 
+
 random();
+
+//Ändrar texten vid elementet med id numberOfGuesses till maxWrong (dvs räknar hur många gissningar som är kvar)
+document.getElementById("wrongGuesses").innerHTML = maxWrong;
+
+//Funktion som ändrar färg när knapparna trycks. Querey letar efter elementet
+function clickedLetter() {
+    document.quereySelector('#a').style.color = "red";
+}
+
+
+//Skapa knapparna
+let html = ``;
+alphabet.forEach(function(letters){
+    html += `<li><button>${letters}</button>`
+})
+console.log(html);
+button.innerHTML = html;
--- readme.md
+++ readme.md
@@ -1,4 +1,12 @@
-Version 1
-[] 
-[]
-[]# u03-JS-hangman
+
+[x] Find random word in array 
+[x] Create letters to click on
+[x] Add images of hangman
+[] Listen to button click
+[] How many letters does the word have
+[] Register picked letters
+[] Create a function to choose word randomly
+[] Create a function who checks if the chosen letter is included in the word
+[] Create a placeholder for letters (amout of guesses)
+[] Show image when wrong
+[] Create reset function
