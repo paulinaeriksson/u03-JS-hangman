580b23e5c2abc234a54b36acadf484395d1a50ba
paulinaeriksson <paulina.eriksson@chasacademy.se>
2021-11-23 15:46:33 +0100

    add functions to buttons

--- js/hangman.js
+++ js/hangman.js
@@ -18,31 +18,33 @@ let word = null;
 let guessed = [];
 let letters = "";
 
+//Array med bokstäver för knapparna
 let alphabet = [
-    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "å", "ä", "ö"
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", 
+    "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "å", "ä", "ö"
 ]
 
 //Funktion för att slumpa fram ord (hundras). Multiplicera med längden på arrayen för att en ny hundras ska slumpas fram varje gång.
-function random(){
+function random() {
     answer = dogs[Math.floor(Math.random() * dogs.length)];
 }
-
-
+//Anropar random
 random();
 
 //Ändrar texten vid elementet med id numberOfGuesses till maxWrong (dvs räknar hur många gissningar som är kvar)
 document.getElementById("wrongGuesses").innerHTML = maxWrong;
 
-//Funktion som ändrar färg när knapparna trycks. Querey letar efter elementet
-function clickedLetter() {
-    document.quereySelector('#a').style.color = "red";
-}
-
-
-//Skapa knapparna
+//Skapa knapparna med onclick-funktion. 
 let html = ``;
 alphabet.forEach(function(letters){
-    html += `<li><button>${letters}</button>`
+    html += `<li><button class="ctrlBtn" onclick="btn" >${letters}</button>`
 })
-console.log(html);
+//Gör elementen synliga i html-dokumentet
 button.innerHTML = html;
+
+//Funktion för att färga varje knapp som använts. 
+document.querySelectorAll('.ctrlBtn').forEach(function(btn){
+    btn.addEventListener('click', function() {
+      this.style.background = "darkgray";
+    });
+  });
--- readme.md
+++ readme.md
@@ -2,9 +2,10 @@
 [x] Find random word in array 
 [x] Create letters to click on
 [x] Add images of hangman
-[] Listen to button click
+[x] Listen to button click
 [] How many letters does the word have
 [] Register picked letters
+[x] Change color of letter box when letter is clicked on
 [] Create a function to choose word randomly
 [] Create a function who checks if the chosen letter is included in the word
 [] Create a placeholder for letters (amout of guesses)
