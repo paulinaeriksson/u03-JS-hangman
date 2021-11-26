2e66a2f90f6b7bf8dd2036d7f7290114b9832d47
paulinaeriksson <paulina.eriksson@chasacademy.se>
2021-11-26 22:03:40 +0100

    clean up code

--- js/hangman.js
+++ js/hangman.js
@@ -1,3 +1,36 @@
+/// VARIABLER ///
+
+//Variabel för button.innerHTML
+let html = ``;
+//Variabel för ordet (hundrasen) man ska gissa på
+let rightWord = "";
+//Variabel för max antal felgissningar
+let maxWrong = 10;
+//Variabel för räknade klick
+let guessCounter = "";
+//Variabel för id:t där det gissade ordet hamnar.
+let word = null;
+//Array för gissade 
+let guessed = [];
+//Variabel för bokstäver
+let letters = "";
+//Variabel för bokstavsknapparna, onclick.
+let btn = "";
+//Variabel för spela igen-knappen
+let resetButton = "";
+//Variabel för bokstäverna som blir över efter att man har gissat
+let leftoverLetters;
+let count = 0;
+
+//Variabel som lagrar data för hur många ggr man har klickat på bokstäverna
+let pressed = "";
+let guess = prompt;
+let splitRightWord = [];
+let index = "";
+let restart = "";
+
+///ARRAYER///
+
 //Array med hundraser
 let dogs = [
     "dalmatiner",
@@ -9,16 +42,45 @@ let dogs = [
     "dobermann",
     "pomeranian",
     "chihuahua"
-]
+];
 
-let rightWord = "";
-let maxWrong = 10;
-let guessCounter = "";
-let word = null;
-let guessed = [];
-let letters = "";
-let btn = "";
-let reset = "";
+//Array med bilder
+let hngImg = document.getElementById("hangman");
+
+let hangmanImgArray = [
+    "./img/bild1.png",
+    "./img/bild2.png",
+    "./img/bild3.png",
+    "./img/bild4.png",
+    "./img/bild5.png",
+    "./img/bild6.png",
+    "./img/bild7.png",
+    "./img/bild8.png",
+    "./img/bild9.png",
+    "./img/bild10.png",
+];
+
+
+
+/*let imageIndex = 0;
+document.querySelectorAll('.ctrlBtn').forEach(function(btn){
+    btn.addEventListener('click', function() {
+        for (let y = 0; y < hangmanImgArray.length; y++) {
+           hngImg.innerHTML = hangmanImgArray[y];
+        }
+    })})*/
+
+
+
+function random() {
+    rightWord = dogs[Math.floor(Math.random() * dogs.length)];
+}
+
+//Anropar random
+random();
+//Test för att se ordet som ska gissas i konsolen
+console.log(rightWord);
+/// SPELPLAN ///
 
 //Array med bokstäver (för knapparna) 
 let alphabet = [
@@ -26,63 +88,89 @@ let alphabet = [
     "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "å", "ä", "ö"
 ]
 
-//Funktion för att slumpa fram ord (hundras). Multiplicera med längden på arrayen för att en ny hundras ska slumpas fram varje gång.
-function random() {
-    rightWord = dogs[Math.floor(Math.random() * dogs.length)];
-}
-//Anropar random
-random();
-
-//Funktion som visar ordet med _ på skärmen genom att dela rightWord, .join tar bort kommatecken mellan varje dold bokstav.
-function displayWord() {
-    document.getElementById("word").innerHTML = rightWord.split("").map(letter => (guessed.indexOf(letter)) >= 0 ? letter : " _ ").join("");
-    
-    //test för att kunna se rätt ord i konsollen - raderas sen.
-    console.log(rightWord);
-    console.log(rightWord.includes("b"));
-   
-}
-
 //Skapa knapparna med onclick-funktion. Tar variabeln html och lägger till html-kod för varje (forEach) bokstav.
-let html = ``;
-alphabet.forEach(function(letters){
-    html += `<li><button class="ctrlBtn" onclick="btn" >${letters}</button>`
+alphabet.forEach(function createLetters(letters){
+    html += `<li><button class="ctrlBtn" onclick="btn" value="${letters}"" >${letters}</button>`
 })
 //Gör button-elementen synliga i html-dokumentet
 button.innerHTML = html;
 
+//Lägger in knapparnas class="ctrlBtn" från html-dokumentet i en variabel
+let ctrlBtn = document.querySelectorAll(".ctrlBtn");
+
 //Funktion för att färga och stänga av varje knapp som använts. 
-document.querySelectorAll('.ctrlBtn').forEach(function(btn){
+ctrlBtn.forEach(function disableBtn (btn){
     btn.addEventListener('click', function() {
       this.style.background = "darkgray";
       btn.disabled = true;
     });
   });
 
-  //Ändrar texten vid elementet med id numberOfGuesses (dvs räknar klick)
-function count () {
-    document.getElementById("guessCounter").innerHTML = guessCounter;
-        }
+//Räknar antal klick på bokstäverna till "Antal gissningar"
+ctrlBtn.forEach(function clickCount (btn){
+    btn.addEventListener('click', function() {
+        document.getElementById("guessCounter").innerHTML++;
+    })})
+
+
+/*ctrlBtn.forEach(function displayImgs() {
+    btn.addEventListener("click", function() {
+        document.getElementById("hangman").src= hangmanImgArray[4];
+    })
     
-//Funktion för ränkaren av antal klick. 
-document.querySelectorAll('.ctrlBtn').forEach(function(btn){
-    btn.addEventListener('click', function() {
-        let pressed = document.getElementById("guessCounter").innerHTML++;
-        if (pressed === 10) {
-            alert("game over");
-        }
-    });
-  });
+});*/
 
-//Funktion för vinst
-//Funktion för förlust
+    
+//En loop för att dölja ordet som ska gissas fram. 
+for (let i = 0; i < rightWord.length; i++) {
+    guessed[i] = "_";
+    console.log(guessed);
+    document.getElementById("word").innerHTML = guessed;
+}
+  
+document.getElementById("word").innerHTML = guessed.join(" ");
+leftoverLetters = rightWord.length;
 
-document.querySelectorAll('.ctrlBtn').forEach(function(btn){
+
+
+//Tar fram index för bokstaven (inte bara första )
+    ctrlBtn.forEach(function getLetterIndex (btn){
     btn.addEventListener('click', function() {
+
+    for (index = rightWord.indexOf(btn.innerHTML); index > -1; index = rightWord.indexOf(btn.innerHTML, index+1)){
+    console.log(index);
+}
+  }  )
+})
+  
+document.getElementById("word").innerHTML = guessed.join(" ");
+leftoverLetters = rightWord.length;
+
+
+
+
+//Gör en array av rightWord
+/*splitRightWord = Array.from(rightWord);
+console.log(Array.from(splitRightWord));
+*/
+
+
+
+
+
+
+//Test på funktion som visar bokstäverna i det dolda ordet
+/*document.querySelectorAll('.ctrlBtn').forEach(function(btn){
+    btn.addEventListener('click', function() {
+     
         let guess = (rightWord.includes(btn.innerHTML))
-       //test
+        let noOfTimes = rightWord.split(btn.innerHTML).length -1;
+        let hiddenWord = document.getElementById("word").innerHTML;
+
         if (guess === true) {
-           console.log(`${btn.innerHTML} finns med i ordet`);
+                        
+          //console.log(rightWord.indexOf(btn.innerHTML));
+           console.log(`${btn.innerHTML} finns med i ordet ${noOfTimes} gånger`);
        } else {
            console.log(`${btn.innerHTML} finns inte med i ordet`);
        };
@@ -91,6 +179,9 @@ document.querySelectorAll('.ctrlBtn').forEach(function(btn){
     });
   });
   
+
+
+
   
 
 //Funktion nollställer antal gissningar
@@ -100,7 +191,15 @@ document.querySelectorAll('.ctrlBtn').forEach(function(btn){
         
     })
 
+//Test för att visa bokstäverna
+function displayGame(){
+for (let i = 0; i = rightWord.length; i++) {
+    console.log(guessed[i] = " _ ");
+ 
+}
 
+//let leftoverLetters = rightWord.length;
+}*/
 
 /*count();*/
-displayWord();
+//displayWord(); 
